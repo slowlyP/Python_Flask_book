8977e16f097358a0f84a6ec31ffd638d01754025
slowlyP <vvckfn@gmail.com>
2026-04-19 21:48:54 +0900

    temp: save work before switching

--- apps/app.py
+++ apps/app.py
@@ -4,12 +4,25 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from apps.config import config
+from flask_login import LoginManager
 
 # SQLAlchemy를 인스턴스화 한다
 db = SQLAlchemy()
 
 
 csrf = CSRFProtect()
+
+# LoginManager를 인스턴스화한다
+login_manager = LoginManager()
+# login_view 속성에 미로그인 시 리다이렉트하는 엔드포인트를 지정한다
+login_manager.login_view = "auth.signup"
+# login_message 속성에 로그인 후에 표시할 메시지를 지정한다
+# 여기에서는 아무것도 표시하지 않도록 공백을 지정한다
+login_manager.login_message = ""
+
+
+
+
 
 # create_app 함수를 작성한다
 def create_app(config_key):
@@ -31,6 +44,9 @@ def create_app(config_key):
 
     # )
 
+    # login_manager를 애플리케이션과 연계한다
+    login_manager.init_app(app)
+
     csrf.init_app(app)
 
     # SQLAlchemy와 앱을 연계한다
@@ -40,11 +56,13 @@ def create_app(config_key):
 
     # crud 패키지로부터 views를 import한다
     from apps.crud import views as crud_views
+    from apps.auth import views as auth_views
 
     # register_blueprint를 사용해 views의 crud를 앱에 등록한다
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
     return app
 
 
-# 143페이지까지함
+# 165페이지까지함
