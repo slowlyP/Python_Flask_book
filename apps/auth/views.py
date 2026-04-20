9c5f12193d706de14ba62e0c6192ff2a7820dbc5
slowlyP <vvckfn@gmail.com>
2026-04-20 12:53:56 +0900

    Finish Chapter 6: Update auth views, templates and detector layout

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -42,7 +42,8 @@ def signup():
         # GET 파라미터에 next 키가 존재하고, 값이 없는 경우는 사용자의 일람 페이지로 리다이렉트한다
         next_ = request.args.get("next")
         if next_ is None or not next_.startswith("/"):
-            next_ = url_for("crud.users")
+            # 회원가입 완료 시의 리다이렉트될 곳을 detector.index로
+            next_ = url_for("detector.index")
         return redirect(next_)
     
     return render_template("auth/signup.html", form=form)
@@ -57,7 +58,7 @@ def login():
         # 사용자가 존재하고 비밀번호가 일치하면 로그인을 허가한다
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
-            return redirect(url_for("crud.users"))
+            return redirect(url_for("detector.index"))
 
         # 로그인 실패 메시지를 설정한다
         flask("메일 주소 또는 비밀번호가 일치하지 않습니다.")
